4ce36626088862161a0e8d51662dc5bc4f448f1e
TIANYUZHOU <1183380395@qq.com>
2022-03-19 20:36:48 +0800

    修改-跨域配置1

--- gra_design_backend/settings.py
+++ gra_design_backend/settings.py
@@ -47,7 +47,8 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
