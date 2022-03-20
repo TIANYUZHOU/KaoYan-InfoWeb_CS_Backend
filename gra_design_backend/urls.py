33b16dd3daf8bcd72f3eba08a52844c8420acc5f
TIANYUZHOU <1183380395@qq.com>
2022-03-20 21:20:37 +0800

    新增-api接口文档生成路由

--- gra_design_backend/urls.py
+++ gra_design_backend/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from django.urls import include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('my_api.urls'))
+    path('api/', include('my_api.urls')),
+    path('docs/', include_docs_urls(title='My API title'))
 ]
-
