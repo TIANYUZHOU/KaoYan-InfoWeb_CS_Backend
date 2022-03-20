2ac3fee1298b4853981fbc51c5ff748f7afed6c6
TIANYUZHOU <1183380395@qq.com>
2022-03-20 20:49:16 +0800

    新增-DRF接口文档配置

--- gra_design_backend/settings.py
+++ gra_design_backend/settings.py
@@ -193,4 +193,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     # 分页
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 接口文档
+'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
