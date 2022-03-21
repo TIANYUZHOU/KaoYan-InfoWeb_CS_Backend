693980ad75d2d7da57032d3f6153818117b1d056
TIANYUZHOU <1183380395@qq.com>
2022-03-21 21:08:21 +0800

    修改-短信发送入口路径

--- verification/urls.py
+++ verification/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import re_path
+from . import views
 
 urlpatterns = [
-    re_path(r'^smscode/(?P<mobile>1[3-9]\d(9)/$)'),
+    re_path(r'^sms_code/(?P<mobile>1[3-9]\d{9})/$',views.SMSCodeView.as_view()),
 ]
