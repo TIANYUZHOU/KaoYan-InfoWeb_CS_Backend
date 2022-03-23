f41cf145111b1c324cf3baa75298308c882017e5
TIANYUZHOU <1183380395@qq.com>
2022-03-23 22:41:10 +0800

    新增-urls

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
-    path('users/',views.UserView.as_view()),
+    path('users/', views.UserView.as_view()),
+    re_path(r'^users/(?P<username>\w{5,20})/count/$', views.UserNameCountView.as_view()),
+    re_path(r'^mobiles/(?P<mobile>1[3-9]\d{9})/count/$', views.MobileCountView.as_view()),
 ]
-
