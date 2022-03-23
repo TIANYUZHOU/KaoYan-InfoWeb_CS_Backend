3a0df299bb320d3ee0ebdb47b3d6cd4fa57b63b0
TIANYUZHOU <1183380395@qq.com>
2022-03-23 22:00:18 +0800

    新增-视图

--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .serializers import CreateUserSerializer
+
 
 # Create your views here.
+
+class UserView(CreateAPIView):
+    """用户注册"""
+    serializer_class = CreateUserSerializer
