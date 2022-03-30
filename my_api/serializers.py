75624b4a3905c43fd66adbc988ae285a5db655aa
TIANYUZHOU <1183380395@qq.com>
2022-03-30 22:15:24 +0800

    修改-分离资料查询接口（因为需要自定义一些配置）

--- my_api/serializers.py
+++ my_api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from user.models import User
 from .models import School, Major, Material
 
 
@@ -10,12 +12,39 @@ class SchoolSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+# 资料-学校外键序列化器
+class MaterialsSchoolSerializer(serializers.ModelSerializer):
+    """定义序列化器"""
+
+    class Meta:
+        model = School
+        fields = ['schName']
+        extra_kwargs = {  # 修改字段选项
+            'schName': {
+                'read_only': True  # 只进行反序列化
+            }
+        }
+
+
+# 资料-用户外键序列化器
+class MaterialsUsersProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+        extra_kwargs = {  # 修改字段选项
+            'username': {
+                'read_only': True  # 只进行反序列化
+            }
+        }
+
+
 class MajorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Major
         fields = '__all__'
 
 
+# 资料上传序列化器
 class MaterialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Material
@@ -26,5 +55,15 @@ class MaterialSerializer(serializers.ModelSerializer):
             },
             'file': {
                 'write_only': True  # 只进行反序列化
-            }
+            },
         }
+
+
+# 资料查询序列化器
+class MaterialInfoSerializer(serializers.ModelSerializer):
+    school = MaterialsSchoolSerializer()
+    user = MaterialsUsersProfileSerializer()
+
+    class Meta:
+        model = Material
+        exclude = ['file']  # 除此字段外
