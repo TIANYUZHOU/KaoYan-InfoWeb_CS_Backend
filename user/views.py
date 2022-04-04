62759b3be3283e0f21fdaeb9b12f220357550a5a
TIANYUZHOU <1183380395@qq.com>
2022-04-04 16:13:45 +0800

    修改-格式化排版

--- user/views.py
+++ user/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializers import CreateUserSerializer, UsersProfileSerializer
-from .serializers import MyTokenSerializer,UserInfoModifySerializer
+from .serializers import MyTokenSerializer, UserInfoModifySerializer
 from .models import User
 
 from rest_framework import status, mixins
